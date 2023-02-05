329538f36729cec2c3c7b8df237533c18dbaf133
jasonrhoads <jasrhoads@gmail.com>
2023-02-05 15:23:58 -0800

    Add basic structure to home page

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from "./Menu"
+
+function Footer() {
+  return (
+    <div>Footer
+        
+        <Menu />
+        
+    </div>
+    
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+import Menu from "./Menu"
+
+function Header() {
+  return (
+    <Header>
+        <Icon></Icon>
+        <Menu />
+    </Header>
+  )
+}
+
+export default Header
+
+const Icon = styled.img`
+`
